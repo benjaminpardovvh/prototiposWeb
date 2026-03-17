a8c0aad1eb8f9d535ee44c4f69dd4cc6158476b6
AATPVVC <angelalvarez@liceovvh.cl>
2026-03-17 14:39:05 -0300

    tarea

---
+++ 01_HOLA_MUNDO/static/js/script.js
@@ -0,0 +1,7 @@
+console.log("conexion exitosa con java script...");
+
+function saludar(){
+    let nombre = prompt("ingresa tu nombre: ")
+    alert("Hola " + nombre)
+    alert("es bueno verte de nuevo mi " + nombre)
+}
